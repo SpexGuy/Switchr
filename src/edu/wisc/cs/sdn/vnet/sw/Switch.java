311f7c0a50c8a829f1748b2d0fe9bb0eba41bdd4
Martin Wickham <spexguy070@gmail.com>
2015-02-27 12:03:49 -0600

    Implement learning switch

--- src/edu/wisc/cs/sdn/vnet/sw/Switch.java
+++ src/edu/wisc/cs/sdn/vnet/sw/Switch.java
@@ -4,12 +4,31 @@ import net.floodlightcontroller.packet.Ethernet;
 import edu.wisc.cs.sdn.vnet.Device;
 import edu.wisc.cs.sdn.vnet.DumpFile;
 import edu.wisc.cs.sdn.vnet.Iface;
+import net.floodlightcontroller.packet.MACAddress;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Aaron Gember-Jacobson
  */
 public class Switch extends Device
-{	
+{
+	private static class MACAddressInfo {
+		public MACAddressInfo(Iface direction) {
+			this.direction = direction;
+			this.createTime = System.currentTimeMillis();
+		}
+		Iface direction;
+		long createTime;
+
+		public boolean isExpired() {
+			return (System.currentTimeMillis() - this.createTime) >= 15000;
+		}
+	}
+
+	private Map<MACAddress, MACAddressInfo> macAddressMap = new HashMap<>();
+
 	/**
 	 * Creates a router for a specific host.
 	 * @param host hostname for the router
@@ -27,11 +46,38 @@ public class Switch extends Device
 	public void handlePacket(Ethernet etherPacket, Iface inIface)
 	{
 		System.out.println("*** -> Received packet: " +
-                etherPacket.toString().replace("\n", "\n\t"));
-		
-		/********************************************************************/
-		/* TODO: Handle packets                                             */
-		
-		/********************************************************************/
+				etherPacket.toString().replace("\n", "\n\t"));
+
+		learnPacket(etherPacket, inIface);
+
+		Iface outgoing = findInterface(etherPacket.getDestinationMAC());
+		if (outgoing != null)
+			sendPacket(etherPacket, outgoing);
+		else
+			broadcast(etherPacket, inIface);
+	}
+
+	private void learnPacket(Ethernet packet, Iface from) {
+		macAddressMap.put(packet.getSourceMAC(), new MACAddressInfo(from));
+	}
+
+	private Iface findInterface(MACAddress addr) {
+		if (macAddressMap.containsKey(addr)) {
+			MACAddressInfo info = macAddressMap.get(addr);
+			if (info.isExpired()) {
+				macAddressMap.remove(addr);
+				return null;
+			}
+			return info.direction;
+		}
+		return null;
+	}
+
+	private void broadcast(Ethernet packet, Iface ignore) {
+		for (Map.Entry<String, Iface> out : getInterfaces().entrySet()) {
+			if (!out.getValue().equals(ignore)) {
+				sendPacket(packet, out.getValue());
+			}
+		}
 	}
 }
